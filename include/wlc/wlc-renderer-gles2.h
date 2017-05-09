e2b4cb2265107f9bba71a9a24b1b6219f707963c
Peter Vanusanik <admin@en-circle.com>
2017-05-09 09:33:38 +0200

    refactor of context, renderer moved to public api

---
+++ include/wlc/wlc-renderer-gles2.h
@@ -0,0 +1,8 @@
+#ifndef _WLC_API_RENDERER_GLES2_H_
+#define _WLC_API_RENDERER_GLES2_H_
+
+#include <wlc/wlc-renderer.h>
+
+void* wlc_gles2(struct wlc_render_api *api);
+
+#endif /* _WLC_API_RENDERER_GLES2_H_ */
